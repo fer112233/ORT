7251d48240b61317165889ff4fa00f79e43886a8
Fer <fpascualsoriano5@gmail.com>
2021-02-16 09:24:13 +0100

    Trabajando 3

--- Main.py
+++ Main.py
@@ -6,3 +6,6 @@ path_to_csv = 'Online Retail_2.csv'
 dt = pd.read_csv(path_to_csv, usecols=['InvoiceNo', 'StockCode', 'Description', 'Quantity', 'InvoiceDate', 'UnitPrice', 'CustomerID', 'Country'])
 
 display(dt)
+
+dt2 = dt['product_type'] = dt['StockCode'].str[0]
+print("Fin")
