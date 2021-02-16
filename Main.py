a46b591f98008206133ce1bd96ea99acf3dbcadb
Fer <fpascualsoriano5@gmail.com>
2021-02-16 09:21:23 +0100

    2a funcion

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
-from IPython import display
+from IPython.display import display, Markdown
 path_to_csv = 'Online Retail_2.csv'
 
-display(pd.read_csv(path_to_csv, usecols=['InvoiceNo', 'StockCode', 'Description', 'Quantity', 'InvoiceDate', 'UnitPrice', 'CustomerID', 'Country']))
+dt = pd.read_csv(path_to_csv, usecols=['InvoiceNo', 'StockCode', 'Description', 'Quantity', 'InvoiceDate', 'UnitPrice', 'CustomerID', 'Country'])
+
+display(dt)
